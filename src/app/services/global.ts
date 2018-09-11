bf69a56365add1e586fb9b9578b21fd52485715b
cesar <cesaranzules7@hotmail.com>
2018-09-11 15:05:04 -0500

    sin cambios

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export var Global ={
-   url:'http://192.168.0.195:3000/api/'
+   url:'http://apiatrium.ecomundo.edu.ec/api/'
 };
 /////http://apiatrium.ecomundo.edu.ec/api/ producion-- http://192.168.0.195:3000/api/ ---desaroollo
